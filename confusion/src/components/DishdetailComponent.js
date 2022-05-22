6921d086cd5a24e80cf28d4cea0249d0dcda50a5
Patrick Swiggers <patrick.swiggers@gmail.com>
2022-05-22 10:08:34 +0200

    review

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -10,13 +10,15 @@ class DishDetail extends Component {
     renderDish(dish) {
         if (dish != null) {
             return (
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                <div className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg top src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
             );
         } else {
             return (
@@ -25,21 +27,23 @@ class DishDetail extends Component {
         }
     };
 
-    renderComments(dish) {
-        if (dish != null) {
-            const comments = dish.comments;
-            const res = comments.map((element) => {
-                return (
-                    <span>
-                        {element.comment}<br />
-                        -- {element.author}<br /><br />
-                    </span>
-                );
-            });
+    renderComments(comments) {
+        if (comments != null) {
             return (
-                <div>
+                <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
-                    {res}
+                    <ul className="list-unstyled">
+                        {comments.map((comment) => {
+                            return (
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>-- {comment.author} , 
+                                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' })
+                                    .format(new Date(Date.parse(comment.date)))}</p>
+                                </li>
+                            );
+                        })}
+                    </ul>
                 </div>
             );
         } else {
@@ -50,17 +54,20 @@ class DishDetail extends Component {
     };
 
     render() {
-        const Dish = this.props.dish;
-        return (
-            <div className="row">
-                <div className="col-md-5 m-1">
-                    {this.renderDish(Dish)}
+        if (this.props.dish != null) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        {this.renderDish(this.props.dish)}
+                        {this.renderComments(this.props.dish.comments)}
+                    </div>
                 </div>
-                <div className="col-md-5 m-1">
-                    {this.renderComments(Dish)}
-                </div>
-            </div>
-        );
+            );
+        } else {
+            return (
+                <div></div>
+            );
+        }
     }
 }
 
